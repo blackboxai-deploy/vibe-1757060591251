c45c163a0a1be380d7b7d5ef3f7b8e32719dbeb2
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 13:53:46 +0530

    Add src/lib/constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,112 @@
+// Taguig City Barangays (28 total)
+export const TAGUIG_BARANGAYS = [
+  "Bagumbayan",
+  "Bambang",
+  "Calzada",
+  "Hagonoy",
+  "Ibayo-Tipas",
+  "Ligid-Tipas",
+  "Lower Bicutan",
+  "New Lower Bicutan",
+  "Napindan",
+  "Palingon",
+  "San Miguel",
+  "Santa Ana",
+  "Tuktukan",
+  "Ususan",
+  "Wawa",
+  "Central Bicutan",
+  "Central Signal Village",
+  "Fort Bonifacio",
+  "Katuparan",
+  "Maharlika Village",
+  "North Daang Hari",
+  "North Signal Village",
+  "Pinagsama",
+  "South Daang Hari",
+  "South Signal Village",
+  "Tanyag",
+  "Upper Bicutan",
+  "Western Bicutan"
+];
+
+// Evacuation Sites
+export const EVACUATION_SITES = [
+  "Evac 1",
+  "Evac 2", 
+  "Evac 3",
+  "Evac 4",
+  "Evac 5"
+];
+
+// Disaster Types
+export const DISASTER_TYPES = [
+  "Flood",
+  "Fire", 
+  "Earthquake"
+];
+
+// Monthly Income Ranges (PHP)
+export const MONTHLY_INCOME_RANGES = [
+  "0 - 5,000",
+  "5,001 - 10,000",
+  "10,001 - 15,000", 
+  "15,001 - 20,000",
+  "20,001 - 25,000",
+  "25,001 - 30,000",
+  "30,001 - 35,000",
+  "35,001 - 40,000",
+  "40,001 - 45,000",
+  "45,001 - 50,000",
+  "50,001 and above"
+];
+
+// Health Priority Levels
+export const HEALTH_LEVELS = [
+  "Normal",
+  "Priority",
+  "High Priority"
+];
+
+// Special Codes
+export const SPECIAL_CODES = [
+  "N/A",
+  "Elderly",
+  "PWD", 
+  "Pregnant",
+  "Lactating",
+  "Infant",
+  "Children"
+];
+
+// Sex/Gender Options
+export const SEX_OPTIONS = [
+  "Male",
+  "Female"
+];
+
+// Common Relations
+export const FAMILY_RELATIONS = [
+  "Spouse",
+  "Son",
+  "Daughter", 
+  "Father",
+  "Mother",
+  "Brother",
+  "Sister",
+  "Grandfather",
+  "Grandmother",
+  "Uncle",
+  "Aunt",
+  "Cousin",
+  "Nephew",
+  "Niece",
+  "Son-in-law",
+  "Daughter-in-law",
+  "Father-in-law",
+  "Mother-in-law",
+  "Brother-in-law",
+  "Sister-in-law",
+  "Grandchild",
+  "Other"
+];
